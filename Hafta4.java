e43564da27e3e2b3ae3263a826726099fd70ef31
SergenAkkaya <akkayasergen384@gmail.com>
2025-11-11 11:09:36 +0300

    Update Hafta4.java

--- Hafta4.java
+++ Hafta4.java
@@ -3,7 +3,7 @@ public class Hafta4 {
         //for (int i = 0; i <=10; i++) {
         //    System.out.println(i);
         //}
-        int i=0;
+        /*int i=0;
         while (i<10) {
             System.out.println(i);
             i++;
@@ -17,5 +17,46 @@ public class Hafta4 {
             j++;
         }
         System.out.println(toplam);
+        System.out.println("---------------------------------");
+
+        int toplam_2=1;
+        int k=1;
+        int faktoriyel=7;
+        while (k<=faktoriyel) {
+            toplam_2*=k;
+            k++;
+        }
+        System.out.println(toplam_2);
+        
+        System.out.println("---------------------------------");
+        int toplam_3=1;
+        int a=2;
+        int b=4;
+        int l=0;
+        while(l<b){
+            toplam_3*=a;
+            l++;
+        }
+        System.out.println(toplam_3);
+        
+        System.out.println("---------------------------------");
+        int sayi=30494;
+        while(sayi !=0){
+        int rakam = sayi%10;
+        System.out.println(rakam);
+        sayi /=10;
+        }
+        System.out.println("---------------------------------");
+        */
+        System.out.println("---------------------------------");
+        int sayi=30494;
+        int toplam_sayi=0;
+        while(sayi !=0){
+        int rakam = sayi%10;
+        toplam_sayi+=rakam;
+        sayi /=10;
+        }
+        System.out.println(toplam_sayi);
+        System.out.println("---------------------------------");
     }
 }
